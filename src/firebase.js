98b07e3922187eb15a007397babc376fd68921ae
MirzaCickusic <56160169+MirzaCickusic@users.noreply.github.com>
2020-09-16 19:49:42 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 try {
     const app = {
-        apiKey: "AIzaSyCNk5CmUx8Hm6TdO5p_jJQqfD-d0RGWBy8",
-        authDomain: "nestovsnesto.firebaseapp.com",
-        databaseURL: "https://nestovsnesto.firebaseio.com",
-        projectId: "nestovsnesto",
-        storageBucket: "nestovsnesto.appspot.com",
-        messagingSenderId: "332679665715",
-        appId: "1:332679665715:web:e0670ab531cf0b31d76b46",
-        measurementId: "G-P0QEGS6J39"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     };
     firebase.initializeApp(app);
 
